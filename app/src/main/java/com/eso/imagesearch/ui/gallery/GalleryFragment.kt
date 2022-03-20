2eee128ccbe6c9add930719edae65d63b54ffce4
Eslam Gamal <eslam.gamal0114@gmail.com>
2022-03-20 19:49:46 +0200

    6_PagingSource_PagingData

--- app/src/main/java/com/eso/imagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/eso/imagesearch/ui/gallery/GalleryFragment.kt
@@ -1,10 +1,23 @@
 package com.eso.imagesearch.ui.gallery
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.eso.imagesearch.R
+import com.eso.imagesearch.data.GalleryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class GalleryFragment :Fragment(R.layout.fragment_gallery){
 
+    private val viewModels by viewModels<GalleryViewModel>()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        viewModels.photos.observe(viewLifecycleOwner){
+
+        }
+    }
 }
